81e9873d40d4f569613726612f68c75b8fc6867c
Jeff Lewis <lewisj88@mac.com>
2016-11-14 17:56:42 -0600

    update lcms for sha256

--- files/brews/little-cms2.rb
+++ files/brews/little-cms2.rb
@@ -3,7 +3,7 @@ require 'formula'
 class LittleCms2 < Formula
   homepage 'http://www.littlecms.com/'
   url 'http://sourceforge.net/projects/lcms/files/lcms/2.4/lcms2-2.4.tar.gz'
-  sha1 '9944902864283af49e4e21a1ca456db4e04ea7c2'
+  sha256 '2a514d10eff26901505c191a15c2588c751eb7fb86577118f6c235aff0be37e8'
 
   version '2.4-boxen1'
 
